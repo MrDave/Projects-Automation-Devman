809761f77fab8dcdaff04202872104b7efd46343
MrDave <davidschnoll@gmail.com>
2023-10-24 15:07:55 +0300

    Create Student model

--- automation/admin.py
+++ automation/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "level",
+        "is_from_far_east",
+    )
--- automation/models.py
+++ automation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField("имя студента", max_length=200)
+    STUDENT_LEVELS = [
+        ("newbie", "новичок"),
+        ("newbie+", "новичок+"),
+        ("junior", "джун"),
+    ]
+    level = models.CharField("уровень студента", choices=STUDENT_LEVELS, default="newbie", max_length=8)
+    is_from_far_east = models.BooleanField("студент с Дальнего Востока", default=False)
+
+    def __str__(self):
+        return self.name
+

db1aa6a734db57eb248682dca320cbda44b490b3
kodunaiff <kodunaiff@gmail.com>
2023-10-30 01:08:36 +0800

    добавил заполнения по ПМу

--- automation/forms.py
+++ automation/forms.py
@@ -1,4 +1,4 @@
-from .models import Student, DevmanUser
+from .models import Student, DevmanUser, ProjectManager
 from django.forms import ModelForm, TextInput,Select, DateTimeInput, Textarea
 
 
@@ -16,9 +16,28 @@ class DevmanUserForm(ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Телеграмм ID'
             }),
-             "email": TextInput(attrs={
-                'class': 'form-control'
+            "email": TextInput(attrs={
+                'class': 'form-control',
+                 'placeholder': 'электронная почта'
             }),
+        }
 
+class ProjectManagerForm(ModelForm):
+    class Meta:
+        model = ProjectManager
+        fields = ['first_name', 'telegram_id', 'email']
 
+        widgets = {
+            "first_name": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'ФИО'
+            }),
+            "telegram_id": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Телеграмм ID'
+            }),
+            "email": TextInput(attrs={
+                'class': 'form-control',
+                 'placeholder': 'электронная почта'
+            }),
         }
--- automation/views.py
+++ automation/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Student
-from .forms import DevmanUserForm
+from .models import DevmanUser
+from .forms import DevmanUserForm, ProjectManagerForm
 
 def index(request):
     return render(request, 'index.html')
@@ -26,8 +26,23 @@ def students(request):
 
 
 def pms(request):
-    return render(request, 'pms.html')
+    error = ''
+    if request.method == 'POST':
+        form = ProjectManagerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ProjectManagerForm()
+    data = {
+        'form': form,
+        'error': error
+
+    }
+    return render(request, 'pms.html', data)
 
 def info_pro(request):
-    students_all = Student.objects.all()
+    students_all = DevmanUser.objects.all()
     return render(request, 'info_pro.html', {'students_all': students_all})
